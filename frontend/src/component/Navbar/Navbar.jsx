ca11094315d28fdc768a738ffd3a693563bcce15
洪榆傑 <z20216705@gmail.com>
2024-08-29 23:43:59 +0800

    navbar change to responsive

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -6,11 +6,11 @@ import {StoreContext} from "../../context/StoreContext.jsx";
 const Navbar = () => {
 
     const {menu,setMenu,cartItems} = useContext(StoreContext);
+    const [isPhoneMenu,setIsPhoneMenu] = useState(false);
 
     useEffect(() => {
-        console.log(cartItems)
-        console.log(Object.keys(cartItems).length)
-    }, [cartItems]);
+        console.log(isPhoneMenu)
+    }, [isPhoneMenu]);
 
     return (
         <div className='navbar'>
@@ -19,17 +19,29 @@ const Navbar = () => {
                     <a href='/'><span className="material-symbols-outlined">ramen_dining</span><span>Umami House</span></a>
                 </div>
 
-                <ul className='navbar-menu'>
+                <ul className={isPhoneMenu?"navbar-menu phone":"navbar-menu"}>
                     <Link to='/' onClick={() => {
+                        if (isPhoneMenu){
+                            setIsPhoneMenu(false)
+                        }
                         setMenu('home')
                     }} className={menu === "home" ? "active" : ""}>回首頁</Link>
                     <Link  to='/menu-introduce' onClick={() => {
+                        if (isPhoneMenu){
+                            setIsPhoneMenu(false)
+                        }
                         setMenu('menu')
                     }} className={menu === "menu" ? "active" : ""}>菜單介紹</Link >
                     <a href='#app-download' onClick={() => {
+                        if (isPhoneMenu){
+                            setIsPhoneMenu(false)
+                        }
                         setMenu('mobile-app')
                     }} className={menu === "mobile-app" ? "active" : ""}>APP</a>
                     <a href='#footer' onClick={() => {
+                        if (isPhoneMenu){
+                            setIsPhoneMenu(false)
+                        }
                         setMenu('contact us')
                     }} className={menu === "contact us" ? "active" : ""}>聯絡我們</a>
                 </ul>
@@ -38,7 +50,8 @@ const Navbar = () => {
                     <span className="material-symbols-outlined">search</span>
                     <div className='shopping-bag'>
                         <span className="material-symbols-outlined">shopping_bag</span>
-                        <div className={(Object.values(cartItems).every(value => value === 0) || Object.keys(cartItems).length === 0)?"":"dot"}></div>
+                        <div
+                            className={(Object.values(cartItems).every(value => value === 0) || Object.keys(cartItems).length === 0) ? "" : "dot"}></div>
                     </div>
                     <div className='nav-profile'>
                         <span className="material-symbols-outlined">person</span>
@@ -48,8 +61,11 @@ const Navbar = () => {
                             <li><span className="material-symbols-outlined">logout</span><span>登出</span></li>
                         </ul>
                     </div>
+                    <span onClick={() => {setIsPhoneMenu((prev) => (!prev))}} className="material-symbols-outlined hamburger">menu</span>
                 </div>
             </div>
+
+
 
         </div>
     )
