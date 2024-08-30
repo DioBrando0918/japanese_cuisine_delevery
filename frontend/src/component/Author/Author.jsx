6dc4e861f2229b40b88623bae26f9bf026cfd432
洪榆傑 <z20216705@gmail.com>
2024-08-30 12:38:35 +0800

    change footer, author, app download to responsive

--- frontend/src/component/Author/Author.jsx
+++ frontend/src/component/Author/Author.jsx
@@ -4,7 +4,7 @@ import './Author.css'
 const Author = () => {
     return (
         <div className='author'>
-                <p><span>©2024 Copyrights Umami House All Rights Reserved | </span>網站系統建置 <span>By</span> 洪榆傑</p>
+            <p><span>©2024 Copyrights Umami House All Rights Reserved | </span>網站系統建置 <span>By</span> 洪榆傑</p>
         </div>
     )
 }
--- frontend/src/component/Footer/Footer.jsx
+++ frontend/src/component/Footer/Footer.jsx
@@ -10,12 +10,12 @@ const Footer = () => {
 
     return (
         <div className='footer' id='footer'>
-            <div className='head'>
+            <div className='footer-head'>
                 <Link to='/'><span className="material-symbols-outlined">ramen_dining</span>
                     <span>Umami House</span></Link>
             </div>
 
-            <div className='body'>
+            <div className='footer-body'>
                 <ul className='link'>
                     <li><a href="/" onClick={()=>{setMenu('home')}}>回首頁</a></li>
                     <li><Link to="/menu-introduce" onClick={()=>{setMenu('menu')}}>菜單介紹</Link></li>
@@ -26,7 +26,7 @@ const Footer = () => {
                 <p>體驗頂級美食，享受舒適就餐環境，滿足您味蕾的極致享受。</p>
             </div>
 
-            <div className='app'>
+            <div className='apps'>
                 <img src={assets.facebook_icon} alt=""/>
                 <img src={assets.instergram_icon} alt=""/>
                 <img src={assets.line_icon} alt=""/>
